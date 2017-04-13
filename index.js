1b578a1330e365dedf3f15a27c685268795d3b3b
SHEFFcode <sheff@sheffmachine.com>
2017-04-13 15:48:51 -0700

    asdads

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ console.log('this is a test');
 console.log('this is a test');
 console.log('this is a test');
 console.log('this is a test');asdasda
-console.log('this is a test');
+console.log('this is a test');asdasdasdsadadsa
 adsasdasasdasdasasdasdas
