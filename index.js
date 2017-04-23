39be7edcf8a64a7ae47a20ed8ee374a87a79d537
SHEFFcode <sheff@sheffmachine.com>
2017-04-22 22:22:29 -0700

    some stuff

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log('this is a test');
 console.log('this is a test');
 console.log('this is a test');asdasda
 console.log('this is a test');
-console.log('this is a test');
+console.log('this is a test');asdasdasdasdas
 console.log('this is a test');
 console.log('this is a test');
 console.log('this is a test');
