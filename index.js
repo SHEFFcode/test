ea5c8821b6884840b50453478ebddca0ba526b17
SHEFFcode <sheff@sheffmachine.com>
2017-05-09 20:22:31 -0700

    asdad

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log('this is a test');
 console.log('this is a test');
 console.log('this is a test');asdasda
 console.log('this is a test');
-console.log('this is a test');asdasdasdasdasasdasdas
+console.log('this is a test');asdasdasdasdasasdasdassdasdsa
 console.log('this is a test');
 console.log('this is a test');
 console.log('this is a test');
